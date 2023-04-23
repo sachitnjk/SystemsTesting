3bf6413527864f6c760e09aa2cac1ac9a9023d40
Sachit Nanajkar <sachitnjk@gmail.com>
2023-04-24 00:31:19 +0530

    Added ItemObjectCaller & PlayerInventoryCaller script whcih call the ItemObject SO and InventoryObject constructor, added prefabs with ItemObjecCaller & PlayerInventoryCaller

--- SystemsTesting/Assets/Scripts/PlayerInventoryCaller.cs
+++ SystemsTesting/Assets/Scripts/PlayerInventoryCaller.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class PlayerInventoryCaller : MonoBehaviour
 {
 	public InventoryObject inventory;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		var item = other.GetComponent<ItemObjectCaller>();
+		if(item)
+		{
+			inventory.AddItem(item.item, 1);
+			Destroy(other.gameObject);
+		}
+	}
+
+	private void OnApplicationQuit()
+	{
+		inventory.Container.Clear();
+	}
 }
